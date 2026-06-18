0ebe7b981a4b3e2ae1ef289496d15ce21a25f8ea
Sumit Tandon <sumit.tandon@oattravel.com>
2026-06-18 12:57:58 +0530

    Added Error page

---
+++ app/error.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Link from 'next/link'
+
+export default function ErrorPage() {
+  return (
+    <>
+      <h1> Error Page</h1>
+      <Link href={'/'}>Home</Link>
+    </>
+  )
+}
